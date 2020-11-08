4c3d28026216ec69de2fa78c8e0243186cd11bf5
shs <2441323532@qq.com>
2020-11-08 10:32:57 +0800

    20201108

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from flask_sqlalchemy import SQLAlchemy
 import os
 import json
 import sys
-import click
 app = Flask(__name__)
 
 date_file='/static/reptile/'
-WIN = sys.platform.startswith('win')
-if WIN:
-    prefix = 'sqlite:///'
-else:
-    prefix = 'sqlite:////'
-app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL',prefix+os.path.join(app.root_path,'user.db'))
-db = SQLAlchemy(app)
 
 
 
