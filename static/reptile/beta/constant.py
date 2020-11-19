fbca87b04752eaf1f5dff75b7dafcbab967a03c2
shs <2441323532@qq.com>
2020-11-19 19:14:32 +0800

    2020111919：10

--- static/reptile/beta/constant.py
+++ static/reptile/beta/constant.py
@@ -22,7 +22,7 @@ page_contains = ['SiteName','SiteDomain','SiteIDCode','ColumnName','ArticleTitle
 pa_contains = ['url','l','r']
 
 def sleep_time_f():
-    return 6+(random.random()-0.5)*1
+    return 7+(random.random()-0.5)*1
 
 def turn_page_on(url,x) :
     return url.rsplit('&',1)[0]+'&page='+str(x)
