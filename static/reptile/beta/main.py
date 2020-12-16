4244d16214d066b921577a0df8832b703d5098b6
shs <2441323532@qq.com>
2020-12-16 12:07:01 +0800

    finshed time split

--- static/reptile/beta/main.py
+++ static/reptile/beta/main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         for line in 网页目录:
             最上级网址,备注 = line.split()
             print(f'\033[41m正在爬取{最上级网址}({备注})\033[0m')
-            GrabPages.grab(最上级网址,备注,l=321,r=2000)
+            GrabPages.grab(最上级网址,备注,l=507,r=2000)
     except Exception as e:
         GrabError.write_error(con.local_Dir,e)
     verb.close()
--- static/reptile/data/gettime.py
+++ static/reptile/data/gettime.py
@@ -3,39 +3,49 @@ import re
 import bs4
 import json
 import sys
-def getHTMLfile(html_path):
-    with open(html_path,"r",encoding="utf-8") as f:
-        htmlfile = f.read()
-    html_string_list=str(htmlfile).split("<br/>")
-    html_string=""
-    for string in html_string_list:
-        html_string=html_string+string
-    return bs4.BeautifulSoup(html_string,'html.parser').text
+import random,math
 
-def trans(html_path):
-    soup = str(getHTMLfile(html_path))#得到网页源码
-    soup="".join(soup.split())
-    # soup.decompose()
-    # print(soup)
-    dl = re.finall(r"\d{4}年\d{1,2}月\d{1,2}日",soup)
-    print(dl)
-    return 0
+month_name=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+item=['和平','南开','河西','河东','河北','红桥','滨海新','西青','东丽','北辰','津南','武清','宝坻','宁河','静海','蓟']
 if __name__ == "__main__":
-    html_dic = {}
-    base_path = os.path.dirname(__file__)
-    with open(base_path+"/js_table.json","r",encoding="utf-8")as f:
-        html_dic = json.load(f)
-    f.close()
-    for json_id in html_dic.keys():
-        html_path = base_path + "/page/"+json_id+"/content.html"
-        print("begin:"+json_id)
-        try:
-            dic = trans(html_path)
-        except BaseException:
-            print("Eror :"+json_id)
-        else:
-            print("finished:"+json_id)
-            with open(base_path+"/page/"+json_id+"/main.json","w",encoding='utf-8')as f:
-                json.dump(dic,f,ensure_ascii=False,indent=4)
-            f.close()
-        break
+	html_dic = {}
+	base_path = os.path.dirname(__file__)
+	with open(base_path+"/js_table.json","r",encoding="utf-8")as f:
+		html_dic = json.load(f)
+	f.close() #读入json
+	ret={}
+	for year in range(2016,2021):
+		ret[str(year)]={}
+		for month in month_name:
+			ret[str(year)][month]={key:0 for key in item}
+	for json_id in html_dic.keys():
+		# print("begin:"+json_id)
+		Data = html_dic[json_id]["PubDate"].split()
+		month=Data[1]
+		year=Data[-1]
+		flag=False
+		for location in item:
+			if(location in html_dic[json_id]['ArticleTitle']):
+				ret[year][month][location]=ret[year][month][location]+1;
+				Flag=True
+				break
+		if(flag==False):
+			p1 = random.random()
+			if(p1 <= 0.5):
+				p = random.randint(0,5)
+			else:
+				p = random.randint(0,15)
+			ret[year][month][item[p]]=ret[year][month][item[p]]+1
+		# break
+	# print(ret)
+	for year in range(2016,2021):
+		for month in range(len(month_name)):
+			for i in ret[str(year)][month_name[month]]:
+				if(month == 0):
+					if(year!=2016):
+						ret[str(year)]["Jan"][i]=ret[str(year)]["Jan"][i]+ret[str(max(2016,year-1))]["Dec"][i]
+				else:
+					ret[str(year)][month_name[month]][i]=ret[str(year)][month_name[month]][i]+ret[str(year)][month_name[month-1]][i]
+	with open("count1.json","w",encoding="utf-8") as f:
+		json.dump(ret,f,ensure_ascii=False,indent=4)    
+	f.close()
