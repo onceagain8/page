62e325c58a3ff5a69e1738b5cd614f31e8799fdd
shs <2441323532@qq.com>
2020-12-22 18:54:48 +0800

    4000 page

--- static/reptile/beta/main.py
+++ static/reptile/beta/main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         for line in 网页目录:
             最上级网址,备注 = line.split()
             print(f'\033[41m正在爬取{最上级网址}({备注})\033[0m')
-            GrabPages.grab(最上级网址,备注,l=3483,r=4000)
+            GrabPages.grab(最上级网址,备注,l=4000,r=4457)
     except Exception as e:
         GrabError.write_error(con.local_Dir,e)
     verb.close()
